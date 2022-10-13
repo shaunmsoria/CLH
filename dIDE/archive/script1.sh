325bb57c2e10f76ebd94e7b738188a08b223d593
shaunmsoria <shaunmsoria@gmail.com>
2022-10-13 20:39:19 +0000

    restructuring the CLH repo

--- dIDE/archive/script1.sh
+++
@@ -1,5 +0,0 @@
-read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
-while : ; do
-set PULSE_PORT "`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
-ss -lpn | grep -q ":$PULSE_PORT " || break
-done
