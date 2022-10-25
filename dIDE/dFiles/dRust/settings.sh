9be125fd33756b4562a5675cf7f5129924bda6a0
shaunmsoria <shaunmsoria@gmail.com>
2022-10-26 07:48:16 +1100

    Update to the dRust init.vim and settings.sh

--- dIDE/dFiles/dRust/settings.sh
+++ dIDE/dFiles/dRust/settings.sh
@@ -14,3 +14,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 source "$HOME/.cargo/env" # Not working from script, need to be executed from terminal
+
+# Install cargo-expand
+cargo install cargo-expand ## TO BE TESTED
