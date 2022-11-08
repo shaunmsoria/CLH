a10635750b74a96c5ccfd69f2d5233e7408b7faa
shaunmsoria <shaunmsoria@gmail.com>
2022-11-09 06:07:35 +1100

    Installing Ganache / Truffle / Hardhat pt1

--- dIDE/dFiles/dRust/settings.sh
+++ dIDE/dFiles/dRust/settings.sh
@@ -13,22 +13,11 @@ sudo apt install -y build-essential
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-<<<<<<< HEAD
 # add cargo directory to bash.bashrc
 sudo printf "\n#add directory cargo to bash\nsource \"$HOME/.cargo/env\"" >> /etc/bash.bashrc
 
 # Install cargo-expand
 cargo install cargo-expand 
-=======
-<<<<<<< HEAD
-#source "$HOME/.cargo/env" 
-
-# add cargo directory to bash.bashrc ## TO BE TESTED
-sudo printf "\n#add directory cargo to bash\nsource \"$HOME/.cargo/env\"" >> /etc/bash.bashrc
-=======
-source "$HOME/.cargo/env" # Not working from script, need to be executed from terminal
 
 # Install cargo-expand
 cargo install cargo-expand ## TO BE TESTED
->>>>>>> 9be125fd33756b4562a5675cf7f5129924bda6a0
->>>>>>> cc1bcbb80aa19b10ad2b3277328375294a50316e
