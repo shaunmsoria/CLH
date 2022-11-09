cc78161145c129c3cceedfe50194d84b621ac303
shaunmsoria <shaunmsoria@gmail.com>
2022-11-10 07:52:36 +1100

    uploading large file to git

--- dIDE/dFiles/dNode/settings.sh
+++ dIDE/dFiles/dNode/settings.sh
@@ -14,6 +14,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # add cargo directory to bash.bashrc
+source "$HOME/.cargo/env"
 sudo printf "\n#add directory cargo to bash\nsource \"$HOME/.cargo/env\"" >> /etc/bash.bashrc
 
 # Install cargo-expand
