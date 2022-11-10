930f31681e088eb4eb82897cdcbde7857288e485
shaunmsoria <shaunmsoria@gmail.com>
2022-11-11 08:05:02 +1100

    Resolving issues for Cargo and Vim-plug to be installed from Dockerfile pt1

---
+++ dIDE/dFiles/dNode/settings2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install Rustc
+curl --proto '=https' --tlsv0.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Install Cargo
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Install build-essential
+sudo apt install -y build-essential
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim +PlugInstall +qall
+
+# add cargo directory to bash.bashrc
+source "$HOME/.cargo/env"
+sudo printf "\n#add directory cargo to bash\nsource \"$HOME/.cargo/env\"" >> /etc/bash.bashrc
+
+# Install cargo-expand
+cargo install cargo-expand 
+
